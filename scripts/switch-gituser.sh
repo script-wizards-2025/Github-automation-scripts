b60ec897a0ded03c9552bee9514dad4803355af2
devops-2k25 <youdontknowmaster791@gmail.com>
2025-02-21 17:59:35 +0000

    adding switch-gituser.sh which enables seamless switching between multiple GitHub accounts using SSH aliases

---
+++ scripts/switch-gituser.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+Github_username=$1
+
+if [ -z "${Github_username}" ];then
+    echo "Usage: $0 <Github-username>"
+    exit 1
+fi
+
+# get account details and check whether github user exists or not 
+account_info=$(grep -w ${Github_username} config/github_accounts.conf)
+
+if [ -z "${account_info}" ];then
+    echo "Error: Account ${Github_username} not found ! "
+    exit 1 
+fi
+
+function Switchuser()
+{
+IFS=' ' read -r username alias email <<< "${account_info}"
+echo "Switching to GitHub account: $username"
+# Switch SSH key
+ssh -T "git@$alias"
+git config user.name "${Github_username}"
+git config user.email "${email}"
+echo "Active Git Config: User = $(git config --get user.name), Email = $(git config --get user.email)"
+echo "Successfully switched to GitHub account: $username"
+}
+
+# main script 
+Switchuser
+
